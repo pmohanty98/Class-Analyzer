e3f8c6584c7a0b231e364633ac008f5f9020b99c
Pinaki Mohanty <pinakim@pal-nat186-159-163.itap.purdue.edu>
2019-04-17 19:13:11 -0400

    working on graphs

--- second.js
+++ second.js
@@ -1,7 +1,27 @@
 
-function makegraph(  mid, final,average,name)
+function plotdatapoints(  studentname,midscore,finalscore)
 {
+object={};
 
+object.showInLegend=true;
+object.name=studentname;
+object.type="spline";
+object.dataPoints=[];
+object.dataPoints.object1={};
+object.dataPoints.object1.label="Midterm";
+object.dataPoints.object1.y=midscore;
+
+    object.dataPoints.object2={};
+    object.dataPoints.object2.label="Final";
+    object.dataPoints.object2.y=finalscore;
+
+return object;
+
+}
+
+function makearray ( finalarray,object)
+{
+    finalarray.push(object);
 
 
 }
