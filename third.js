5b383b1d36e210952ce73aaa59d522de34d82387
Pinaki Mohanty <pinakim@Pinakis-MacBook-Pro.local>
2019-04-21 15:02:33 -0400

    almost done

--- third.js
+++ third.js
@@ -11,10 +11,10 @@ firebase.initializeApp(config);
 database = firebase.firestore();
 
 
-function scoreupdation() {
+ function scoreupdation() {
 
     var stuname = document.getElementById("stu_name").value;
-    var newscore = document.getElementById("newscore").value;
+    var newscore = parseInt(document.getElementById("newscore").value);
     var sel = document.getElementById('box1');
     var exam = sel.options[sel.selectedIndex].value;
     //  console.log(exam);
@@ -29,12 +29,10 @@ function scoreupdation() {
 
     var found=0;
     var stuID;
-    database.collection("Student").get().then(function (StudentSnapshot) {
+    database.collection("Student").get().then( function (StudentSnapshot) {
 
 
-
-
-        StudentSnapshot.forEach(  function (Student) {
+        StudentSnapshot.forEach( function (Student) {
 
             var RETRIEVEDname = Student.data().Name;
 
@@ -44,48 +42,41 @@ function scoreupdation() {
             }
         });
 
+        if(found==0)
+            alert("NO SUCH STUDENT REGISTERED."+"\n"+"Try Again.");
 
-              if(found==1) {
+       else if(found==1 && examentry=="Midterm1") {
+           var ref = database.collection("Student").doc(stuID);
+           console.log(ref);
+           ref.update({
+               "Midterm1": newscore
+           });
+            alert("SUCCESS") ;
+       }
+        else if(found==1 && examentry=="Midterm2") {
+            var ref = database.collection("Student").doc(stuID);
+            console.log(ref);
+            ref.update({
+                "Midterm2": newscore
+            });
+            alert("SUCCESS") ;
+        }
+        else if(found==1 && examentry=="Final") {
+            var ref = database.collection("Student").doc(stuID);
+            console.log(ref);
+            ref.update({
+                "Final": newscore
+            });
 
-                  console.log(stuID);
-                  console.log(found);
-                  var ref = database.collection("Student").doc(stuID);
-                  console.log(ref);
-
-                  if (examentry == "Midtrem1") {
-
-                      ref.update({
-                          "Midterm1": newscore
-                      }).then(function () {
-                          console.log("Document successfully updated!");
-                          alert("SUCCESS");
-                      });
-
-                  } else if (examentry == "Midtrem2") {
-
-                      database.collection("Student").doc(stuname).update({
-                          "Midterm2": newscore
-                      }).then(function () {
-                          console.log("Document successfully updated!");
-                          alert("SUCCESS");
-                      });
-
-                  } else if (examentry == "Final") {
-
-                      database.collection("Student").doc(stuname).update({
-                          "Final": newscore
-                      }).then(function () {
-                          console.log("Document successfully updated!");
-                          alert("SUCCESS");
-                      });
-
-                  }
+            alert("SUCCESS") ;
+        }
 
 
-              }
+    });
 
-        });
+
+  }
 
 
 
-}
+
